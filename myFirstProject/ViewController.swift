927070476b38c95630da51cee31892d2a4518582
Sergey Patrakov <sergeypatrakov@MacBook-Pro-Sergey.local>
2023-07-23 17:45:03 +0300

    Firstcommit

---
+++ myFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstProject
+//
+//  Created by Sergey Patrakov on 23.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
